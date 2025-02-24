52aacdc3e6f29c5dc6d37ffa03fa5894a3236bf3
nyovhz <nyovhz.dev@gmail.com>
2025-02-24 11:50:03 -0600

    mod

--- src/app/modules/core.js
+++ src/app/modules/core.js
@@ -27,16 +27,16 @@ export default function XNeuron() {
       />
 
       {/* INFO */}
-      <div className="absolute inset-0 flex items-center justify-center p-6">
-        <div className="bg-black/35 backdrop-blur-md text-green-100 max-w-7xl p-8 rounded-2xl shadow-xl text-center">
-          <h1 className="text-3xl font-bold mb-6 text-green-600">AI Parasytes</h1>
-            <p className="text-2xl">
+      <div className="absolute inset-0 flex items-center justify-center p-6 ">
+        <div className="bg-black/30 backdrop-blur-xs text-white max-w-7xl p-8 rounded-2xl drop-shadow-lg text-center">
+          <h1 className="text-lg md:text-5xl font-bold mb-6 text-green-500 shadow-lg rounded-3xl bg-black/20">AI Parasytes</h1>
+            <p className="text-md md:text-3xl">
               El nombre "AI Parasytes" refleja la idea central del proyecto: la IA como un ente que consume,
               modifica y parasitiza la realidad. En este mundo, la máquina no es una herramienta que sirve al humano,
               sino una entidad autónoma que se adueña de las experiencias, alterándolas, distorsionándolas y convirtiéndolas
               en algo ajeno, pero al mismo tiempo profundamente humano.
             </p>
-            <p className="mt-6 text-2xl">
+            <p className="mt-6 text-md md:text-3xl">
               La estética visual del proyecto es un collage de glitches, errores digitales y paisajes generados por IA,
               creando una sensación constante de inestabilidad y tensión. El sonido es igual de disruptivo: sintetizadores,
               frecuencias experimentales y sonidos generados por IA se entrelazan para crear una sonoridad invasiva, que
