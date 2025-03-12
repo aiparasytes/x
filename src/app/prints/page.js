0e908b89c98abcc70a9adfe2f2ea2aebf824782d
nyovhz <nyovhz.dev@gmail.com>
2025-03-12 17:31:19 -0600

    add

--- src/app/prints/page.js
+++ src/app/prints/page.js
@@ -7,7 +7,7 @@ export default function RutaSecundaria() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/prints/x.html');
+    router.push('/prints/7C6C5617E4E62DF76DCC9EB8D1C96F2839AA42B24858FFE6D22EBABBC7F337FB.html');
   }, [router]);
 
   return null;
