b8965eb7f810761dbfc220833fb7ec539e00fd3b
nyovhz <nyovhz.dev@gmail.com>
2025-03-13 01:17:43 -0600

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -9,28 +9,24 @@ import HydraCanvas from './components/hydra';
 import Sidebar from './components/sidebar';
 
 export default function Home() {
-  const [isCameraActive, setIsCameraActive] = useState(false);  // Estado para activar/desactivar la cámara
+  const [isCameraActive, setIsCameraActive] = useState(false);
 
-  // Función para alternar el estado de la cámara
   const toggleCamera = () => {
     setIsCameraActive((prev) => !prev);
     console.log(isCameraActive);
   };
 
-  // Hook para detectar WebView (en este caso Instagram)
   useEffect(() => {
     const isInstagramWebView = navigator.userAgent.includes("Instagram");
 
     if (isInstagramWebView) {
-      // Redirigir a un enlace externo para abrir en un navegador
-      window.location.href = "https://aiparasytes.xyz/";  // URL que quieres abrir en el navegador
+      window.location.href = "https://aiparasytes.xyz/";
     }
   }, []);
-git 
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat relative">
       <section className="relative w-full py-10 z-30">
-        <Header onCameraToggle={toggleCamera} />  {/* Pasamos la función al Header */}
+        <Header onCameraToggle={toggleCamera} />
         <Sidebar />
         <Info />
         <Gallery />
