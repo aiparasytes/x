603470341b841a16e8e933317c02590f9c2b8814
nyovhz <nyovhz.dev@gmail.com>
2025-03-20 23:12:25 -0600

    add: video record

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true, // Asegura que las rutas usen una barra al final
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Aplica a toda la app
-        headers: [
-          {
-            key: "Permissions-Policy",
-            value: "camera=*, microphone=*, geolocation=*, autoplay=*",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none", // 🔥 Permite cargar scripts de otras fuentes
-          },
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "cross-origin", // 🔥 Permite recursos de otros dominios
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src * 'self' blob: data:; script-src * 'unsafe-inline' 'unsafe-eval'; img-src * data: blob:; media-src * blob: data:; connect-src *;",
-          },
-        ],
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
